0d4c07fcbcd939926a59258aa3d610d82c4b02a1
said_sv <sirsuyvi@gmail.com>
2022-10-20 14:09:30 -0500

    un test mas

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:true}),bodyParser.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,22 +20,30 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+  dns.lookup('google.com',(err,add,family)=>{
+    console.log({address:add,fam:family});
+  });
+  res.send('a');
 });
 
 var SHORT = [];
 
-app.post('/api/shorturl',bodyParser.urlencoded({extended:true}),bodyParser.json(),(req,res)=>{
-  var shortUrl = {original_url:req.body.url,shorturl:Math.floor(Math.random()*10000)+1};
-  SHORT.push(shortUrl);
-  res.json(shortUrl);
+app.post('/api/shorturl',(req,res)=>{
+  var {url} = req.body;
+  if(/(http:\/\/|https:\/\/)(w{3}\.|).+/.test(url))
+  {
+    var short = {original_url:url,short_url:Math.floor(Math.random()*10000)+1};
+    SHORT.push(short);
+    res.json(short);
+  }
+  else res.json({error:'invalid url'});
 });
 
 app.get('/api/shorturl/:id',(req,res)=>{
   var {id} = req.params;
   for(shortCurrent of SHORT)
   {
-    if(shortCurrent.shorturl == id) res.redirect(shortCurrent.original_url);
+    if(shortCurrent.short_url == id) res.redirect(shortCurrent.original_url);
     else continue;
   }
   res.json({error:'Hubo un error redirijiendod tu pagina.'});
