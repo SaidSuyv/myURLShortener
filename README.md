23544e1103f83c14fcbca0195eb46b55ccedb4f4
said_sv <sirsuyvi@gmail.com>
2022-10-20 12:35:56 -0500

    update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # URL Shortener Microservice
 
 This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+
+# Where can I find this service online?
+
+You can easily find this project in the following link.
+
+[My URL Shortener](https://boilerplate-project-urlshortener.saidsuyv.repl.co)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
@@ -17,6 +18,24 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+var SHORT = [];
+
+app.post('/api/shorturl',bodyParser.urlencoded({extended:true}),bodyParser.json(),(req,res)=>{
+  var shortUrl = {original_url:req.body.url,shorturl:Math.floor(Math.random()*10000)+1};
+  SHORT.push(shortUrl);
+  res.json(shortUrl);
+});
+
+app.get('/api/shorturl/:id',(req,res)=>{
+  var {id} = req.params;
+  for(shortCurrent of SHORT)
+  {
+    if(shortCurrent.shorturl == id) res.redirect(shortCurrent.original_url);
+    else continue;
+  }
+  res.json({error:'Hubo un error redirijiendod tu pagina.'});
 });
 
 app.listen(port, function() {
